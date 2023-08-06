2bc1297e6cdd63ef841318f197512c885c38764d
shaktilucifer <shakticse@gmail.com>
2023-08-06 09:05:10 -0400

    add update state and hooks for board config

--- app/src/Board.tsx
+++ app/src/Board.tsx
@@ -1,26 +1,64 @@
-import React from "react";
+import React, { useCallback, useEffect, useState } from "react";
 import "./Board.css";
 
-function useBoardConfig() {}
+function useBoardConfig() {
+  const [boardState, setBoardState] = useState([
+    ["x", "x", "x"],
+    ["", "", ""],
+    ["", "", ""],
+  ]);
+  useEffect(() => {
+    console.log(boardState);
+  }, [boardState]);
+
+  const updateBoardState = useCallback(
+    (row: number, col: number, mark: string) => {
+      console.log({ row, col, mark });
+
+      setBoardState((boardState) => {
+        const currentBoardState = [...boardState];
+        currentBoardState[row][col] = mark;
+        return currentBoardState;
+      });
+    },
+    [setBoardState]
+  );
+
+  return {
+    boardState,
+    updateBoardState,
+  };
+}
+
+interface BoardRowProps {
+  rowValues?: string[];
+}
 
 function Board() {
-  function BoardRow() {
+  const { boardState, updateBoardState } = useBoardConfig();
+
+  function BoardRow({ rowValues }: BoardRowProps) {
     return (
       <>
         <div className="boardRow">
-          <div className="boardCell"></div>
-          <div className="boardCell"></div>
-          <div className="boardCell"></div>
+          {rowValues?.map((value: string) => (
+            <div
+              onClick={() => updateBoardState(0, 0, "S")}
+              className="boardCell"
+            >
+              {value}
+            </div>
+          ))}
         </div>
       </>
     );
   }
-
+  console.log({ boardState });
   return (
     <div className="board">
-      <BoardRow />
-      <BoardRow />
-      <BoardRow />
+      <BoardRow rowValues={boardState?.[0]} />
+      <BoardRow rowValues={boardState?.[1]} />
+      <BoardRow rowValues={boardState?.[2]} />
     </div>
   );
 }
